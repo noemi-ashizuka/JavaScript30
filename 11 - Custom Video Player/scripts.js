69f696e3afc59ff5ff9bc71a568ea0a213f53466
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-06-26 13:58:58 +0900

    added play and pause toggle

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,25 @@
+// Select elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtone = player.querySelectorAll('[data-skip');
+const ranges = player.querySelectorAll('.player__slider');
+
+// Build functions
+
+function togglePlay() {
+  if(video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+  // alternative way of doing this by calling string as result of ternary operator:
+  // const method = video.paused ? 'play' : 'pause';
+  // video[method]();
+}
+
+// Connect events listener
+
+video.addEventListener('click', togglePlay);
